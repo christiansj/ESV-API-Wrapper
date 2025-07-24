473da4fabd1438e025b16329048490a628095c65
christiansj <christiansj10@gmail.com>
2025-07-23 21:17:11 -0500

    adding api

---
+++ api/src/server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Root url
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
