0f703cc770f236d1a5ffa632cbb91153329258fe
christiansj <christiansj10@gmail.com>
2025-07-31 09:03:43 -0500

    updating book schema

--- api/src/models/Book.model.ts
+++ api/src/models/Book.model.ts
@@ -8,7 +8,21 @@ import { ResultCallback } from '../types';
 const Book = function () {};
 
 Book.getAll = (resultCallback: ResultCallback) => {
-    const query = "SELECT title, chapter_count as chapterCount FROM book";
+    const query = `
+        SELECT
+            b.title,
+            b.chapter_count as chapterCount,
+            t.name as testamentName,
+            bc.name as categoryName
+        FROM 
+            book b,
+            testament t,
+            book_category bc
+        WHERE (
+            b.testament_id = t.id
+            AND b.book_category_id = bc.id
+        );
+    `;
     executeQuery(query, null, resultCallback);
 }
 
