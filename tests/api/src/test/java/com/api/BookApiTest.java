e5f38dc075dcea573b8a7854fefa96949984f241
christiansj <christiansj10@gmail.com>
2025-07-30 22:03:14 -0500

    adding json schema validation

--- tests/api/src/test/java/com/api/BookApiTest.java
+++ tests/api/src/test/java/com/api/BookApiTest.java
@@ -8,13 +8,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
+import io.restassured.module.jsv.JsonSchemaValidator;
+
 public class BookApiTest {
     private final String API_URL = System.getenv("API_URL");
     private final Logger LOGGER = LogManager.getLogger("bookLogger");
 
     @Test
     public void bookEndpointHealthCheck() {
-        LOGGER.info("Book Endpoint Health Check Test");
+        LOGGER.info("== TEST Book Endpoint Health Check ==");
         given()
             .baseUri(API_URL + "/book")
         .when() 
@@ -22,5 +24,19 @@ public class BookApiTest {
         .then() 
             .statusCode(200);
         LOGGER.info("\"/book\" endpoint returns status code 200");
+    }
+
+    @Test
+    public void bookGetAllSchema(){
+        LOGGER.info("== TEST Get All \"/book\" Schema ==");
+        given()
+            .baseUri(API_URL + "/book")
+        .when() 
+            .get()
+        .then()
+            .assertThat()
+            .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas/books.json"))
+            .log().ifValidationFails();
+        LOGGER.info("Get All \"/book\" returns correct schema");
     }
 }
